65678891f10fc32b6b05631ab3fd308704bd6be9
rmzmrnn <marananremzell@gmail.com>
2024-06-05 21:45:57 +0800

    algorithm works however there is no backtracking

--- sudoku_functions.hpp
+++ sudoku_functions.hpp
@@ -3,5 +3,8 @@
 #include <algorithm>
 
 bool createSudoku(int (&sudoku_arr)[9][9]);
-bool sudokuChecker(int (&sudoku_arr)[9][9]);
+//bool sudokuChecker(int (&sudoku_arr)[9][9]);
+bool rowChecker(int value, int (&sudoku_arr)[9], int col);
+bool colChecker(int value, int (&sudoku_arr)[9][9], int row, int col);
+bool boxChecker(int value, int (&sudoku_arr)[9][9], int row, int col);
 void printSudoku(int (&sudoku_arr)[9][9]);
